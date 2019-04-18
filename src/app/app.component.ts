1eab2ef743f6a8a8250ffcb3c57d6880579e987e
M1050962 <pavanaditya.mylavarapu@mindtree.com>
2019-04-18 13:46:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-another-app';
+  title = 'Developer of this awesome app is';
 }
